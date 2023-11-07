f787892acf5d64e510fd56ae4ef2099897022c27
Joonatan <joonatanliljanto@kamk.fi>
2023-11-07 10:40:18 +0200

    Implement quadratic color interpolation for dynamic circle

--- game.js
+++ game.js
@@ -9,13 +9,13 @@ const EFFECT_DURATION = 15;
 const CYCLE_DURATION = 480; // Duration of the complete grow and shrink cycle in frames
 const BACKGROUND_COLOR = [18, 18, 18];
 const STATIC_CIRCLE_COLOR = [60, 60, 60];
-const DYNAMIC_CIRCLE_COLOR = [70, 130, 180, 100];
+const DYNAMIC_CIRCLE_COLOR = [70, 130, 180, 100]; // Blue
+const ACTIVE_CIRCLE_COLOR = [128, 0, 128, 100]; // Purple
 const EFFECT_COLOR = [0, 255, 100]; // Green
 const SCORE_COLOR = [255];
 
 // State variables
 let dynamicRadius;
-let grow = true;
 let score = 0;
 let effectCounter = 0;
 let maxSize;
@@ -32,7 +32,6 @@ function setup() {
   noStroke();
 }
 
-// Main draw loop
 function draw() {
   background(...BACKGROUND_COLOR);
 
@@ -40,19 +39,35 @@ function draw() {
   fill(...STATIC_CIRCLE_COLOR);
   ellipse(width / 2, height / 2, STARTING_RADIUS * 2);
 
-  updateAndRenderDynamicCircle();
+  renderDynamicCircle();
   renderScore();
-  applyEffectIfNecessary();
+  applyEffect();
 }
 
-function updateAndRenderDynamicCircle() {
+function renderDynamicCircle() {
   let cycleProgress = (frameCount % CYCLE_DURATION) / CYCLE_DURATION;
   let easedProgress = (cos(TWO_PI * cycleProgress) + 1) / 2; // Oscillates between 0 and 1
   dynamicRadius = lerp(minSize, maxSize, easedProgress);
-  grow = sin(TWO_PI * cycleProgress) >= 0;
 
-  fill(...DYNAMIC_CIRCLE_COLOR);
+  let colorAtMaxSize = color(...DYNAMIC_CIRCLE_COLOR); // Blue when the circle is at max or min size
+  let colorAtOptimalSize = color(...ACTIVE_CIRCLE_COLOR); // Purple when it's time to press
+
+  // Determine the proximity to the optimal size
+  let proximity = abs(dynamicRadius - STARTING_RADIUS) / (maxSize - STARTING_RADIUS);
+
+  // Use a quadratic easing for a smoother transition
+  let colorEasedProgress = easeInOutQuad(max(0, 1 - proximity * 2));
+
+  // Lerp the color based on the current size of the circle
+  let currentColor = lerpColor(colorAtMaxSize, colorAtOptimalSize, colorEasedProgress);
+
+  fill(currentColor);
   ellipse(width / 2, height / 2, dynamicRadius * 2);
+}
+
+// https://easings.net/#easeInOutQuad
+function easeInOutQuad(x) {
+  return x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2;
 }
 
 function renderScore() {
@@ -60,7 +75,7 @@ function renderScore() {
   text(`Score: ${score}`, width / 2, SCORE_POSITION_Y);
 }
 
-function applyEffectIfNecessary() {
+function applyEffect() {
   if (effectCounter > 0) {
     fill(...EFFECT_COLOR);
     ellipse(width / 2, height / 2, STARTING_RADIUS * 2);
